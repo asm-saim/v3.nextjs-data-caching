e3c7678e0e42ba852f8b0f0f7c8165b2dd92c800
asm-saim <saim777m@gmail.com>
2026-09-18 19:43:48 +0600

    different types of data fetching process implemented

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,11 +1,45 @@
-import React from "react";
+/**
+ * GET
+ * POST
+ * UPDATE
+ * DELETE
+ */
 
-const page = () => {
+//Fetching data: process 1 (recommended) we can reuse it 
+const getPosts = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return res.json();
+};
+
+//Fetching data: process 3 !ok
+const getPosts3 = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+};
+
+//Fetching data: process 2 try , catch
+const getPosts2 = async () => {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    return res.json();
+  } catch (error) {
+    throw new Error("Failed to fetch posts");
+  }
+};
+
+const Page = async () => {
+  //   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  //   const data = await res.json();
+
+  const postsData = await getPosts();
+
   return (
     <div>
-      <h1>This is Posts page</h1>
+      <h1>Total Data: {postsData.length}</h1>
     </div>
   );
 };
 
-export default page;
+export default Page;
