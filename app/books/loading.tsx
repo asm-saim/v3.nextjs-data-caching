ecda6998c2773b07b1de2071e0730c5d2975cfb6
asm-saim <saim777m@gmail.com>
2026-09-20 19:03:11 +0600

    loader implemented in books

---
+++ app/books/loading.tsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <div className=" text-4xl text-center">
+      <span className="loading loading-spinner text-info"></span>
+    </div>
+  );
+};
+
+export default loading;
